b37cb8bd116d8303d171de39ce68e02ecf9a469b
prashant-paliwal <prashantpaliwal203@gmail.com>
2024-07-01 11:31:23 +0530

    Fixed duplicate docker name issue

--- first_app/views.py
+++ first_app/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to the index page of CICD Project Final!")
+    return HttpResponse("Welcome, This is index page of CICD Project!")
