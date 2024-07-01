fd4fccb91aa192da61f2cc552e468823ce80934d
prashant-paliwal <prashantpaliwal203@gmail.com>
2024-07-01 11:10:50 +0530

    CICD commit

--- first_app/views.py
+++ first_app/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to the index page of CICD Project!!")
+    return HttpResponse("Welcome to the index page of CICD Project Final!")
